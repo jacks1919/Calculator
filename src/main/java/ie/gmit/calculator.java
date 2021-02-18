63e08e03f4d973326104726221df4f81b60dd661
Jacks1919 <g00326495@gmit.ie>
2021-02-18 10:15:17 +0000

    Added Test to test add function.

--- src/main/java/ie/gmit/calculator.java
+++ src/main/java/ie/gmit/calculator.java
@@ -1,8 +1,15 @@
 //  Jack Sheridan
+//  G00326495
 //  Class to add numbers
 //  10-02-21
 
 package ie.gmit;
 
 public class calculator {
+
+    public int add(int firstNum, int secondNum) {
+
+        int result = firstNum + secondNum;
+        return result;
+    }
 }
